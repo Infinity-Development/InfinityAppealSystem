026c19d1f2f51fbe66d6b3a8c0a58c0e4e250884
PrettyChillDev <81452894+ChillCordDev@users.noreply.github.com>
2021-05-26 17:31:02 -0600

    Update Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -6,7 +6,7 @@ import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import {Redirect} from "react-router-dom";
-// import {createJwt} from "../Helpers/jwt-helpers";
+import {createJwt} from "../Helpers/jwt-helpers";
 
 const axios = require("axios")
 
