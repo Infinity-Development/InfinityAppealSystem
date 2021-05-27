02790e465686b39887f4ae620a5befa412b6836d
PrettyChillDev <81452894+ChillCordDev@users.noreply.github.com>
2021-05-26 18:38:49 -0600

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -18,13 +18,26 @@ class Home extends Component {
         return (
             <Grid container alignItems={"center"} justify="center" direction="column">
                 <Grid item xs={12}>
-                    <Button startIcon={<Icon size={1} path={mdiDiscord}/>} href={url} size={"large"} className={"button"}>Login with Discord</Button>
+                    <div className="cus_box">
+                      <h1>Hello There, Welcome</h1>
+                      <p>If you find yourself browsing or using this site, you should be dissapointed. Here at Infinity we strive in providing our users a safe, curtious, drama free community and only ask that you follow a few simple <a href=""> rules </a>.</p>
+                    </div>
+                    <div className="cus_box">
+                      <h1>What can i do?</h1>
+                      <p>If you find yourself here, you should be dissapointed. Here at Infinity we strive in providing our users a safe, curtious, drama free community and you obviously did something to violate our rules or generally piss us off.</p>
+                    </div>
+                    <div className="cus_box">
+                      <h1>Useful Info</h1>
+                      <p>
+                          1. 
+                      <br />
+                          2.
+                      <br />
+                      </p>
+                    </div>
                 </Grid>
                 <Grid item xs={12}>
-                    <div className="delete_box">
-                      <h4>About this Site</h4>
-                      <p>If you find yourself here, you should be dissapointed in yourself.</p>
-                    </div>
+                    <Button startIcon={<Icon size={1} path={mdiDiscord}/>} href={url} size={"large"} className={"button"}>Login with Discord</Button>
                 </Grid>
             </Grid>
         );
