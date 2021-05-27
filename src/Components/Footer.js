b8d566d2de59417346a458182a2f02651c084d1e
PrettyChillDev <81452894+ChillCordDev@users.noreply.github.com>
2021-05-26 19:07:50 -0600

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const Footer = () => {
   return (
-    <MDBFooter color="blue" className="font-small pt-4 mt-4">
+    <MDBFooter color="#23222b" className="font-small pt-4 mt-4">
       <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
           <MDBCol md="6">
